9ad256012dfde42035edcb4ec0c833c8edda8aca
valentinbus <valentinoiho@gmail.com>
2018-11-23 11:01:27 +0100

    Classe maze fonctionnelle affichage ok

--- classes.py
+++ classes.py
@@ -22,20 +22,30 @@ class Maze:
 
     def display_lvl(self, window):
         wall = pygame.image.load('./ressource/structures.png').convert_alpha()
+        start = pygame.image.load('./ressource/personnages.png')
+        texture = pygame.image.load('./ressource/texture.png')
         end = pygame.image.load('./ressource/Gardien.png').convert_alpha()
         #Define where start picture's blit
-        sprite = wall.get_size()
+        sprite = wall.get_size()[0]
         x=0
         y=0
+        len_line = 15*32
         for row in self.structure:
             for column in row:
                 if column == 'm':
                     window.blit(wall, [x, y])
+                elif column == 'a':
+                    window.blit(end, [x, y])
+                else :
+                    window.blit(texture, [x, y])
+
                 x+=sprite
+
                 #When the loop read one line, reboot x position
-                if x >= window.get_size()[0]:
+                if x >= len_line: #15 is the number of sprite in a line of the maze
                     x=0
                     y+=sprite
+
             
 class Character:
     #Character initialisation
@@ -43,20 +53,22 @@ class Character:
         self.position = [1, 1]
 
     def move_character():
+        print()
 
 
 
 
 #### Zone de debug ###
 
+#Initialisation of the maze
 pygame.init()
-window = pygame.display.set_mode((600, 600))
 
 maze = Maze('./ressource/maze_structure')
 maze.structure_construction()
+window = pygame.display.set_mode((15*32, 15*32))
 maze.display_lvl(window)
 
-#BOUCLE INFINIE
+#Starting Game Loop
 continuer = 1
 while continuer:
     pygame.display.update()
--- test.py
+++ test.py
@@ -1,23 +1,35 @@
-import pygame
+class Maze:
+    #Define file use to create lvl structure
+    def __init__(self, file):
+        self.file = file
+        self.structure = []
 
-screen = pygame.display.set_mode([800, 800], 0, 32)
-#initiates screen
+    #Parse file to create structure
+    def structure_construction(self):
+        with open(self.file, 'r') as file:
+            #Define total rows
+            for row in file:
+                row_structure = []
+                #Append total number of case per row
+                for case in row:
+                    if case != '\n':
+                        row_structure.append(case)
+                #Create a table in two dimensions 
+                self.structure.append(row_structure)
 
-image1 = pygame.image.load('./ressource/background.png').convert_alpha()
-#testimage0.jpg is loaded into the variable image1
+maze = Maze('./ressource/maze_structure')
+maze.structure_construction()
 
-image2 = pygame.image.load('./ressource/aiguille.png').convert_alpha()
-#testimage.png is loaded into the variable image2
+x=0
+y=0
 
-while True:
-    screen.fill([0, 0, 0])
-    #screen is filled with a black background
-
-    screen.blit(image1, [200, 200]) 
-    #here image1 is blitted onto screen at the coordinates (200,200)
-
-    image1.blit(image2, [0, 0])
-    #here image2 is blitted onto image1 at the coordinates (0,0) which starts at the upper left of image1
-
-    pygame.display.update()
-    #updates display, which you can just ignore
+for i in maze.structure:
+    for c in i :
+        if c == 'm':
+            print('')
+        x+=1
+        print(x)
+    if x >=15 :
+        x=0
+        y+=1
+        print(y)
